24c29db73335a464814dc414ae7ffb749bc67b0c
DrDanRyan <danryanfl@yahoo.com>
2014-04-23 18:46:41 -0400

    adding on to OCProblem

--- OCProblem.m
+++ OCProblem.m
@@ -45,9 +45,10 @@ classdef OCProblem < handle
       end
       
       
-      function [x, J] = compute_states(obj, u)
+      function [x, J, xK] = compute_states(obj, u)
          x = zeros(obj.nStates, obj.nSteps+1);
          x(:,1) = obj.x0;
+         xK = zeros(obj.nStates, obj.nSteps, 3);
          cumObj = zeros(1, obj.nSteps+1);
          
          t = obj.tspan;
@@ -56,22 +57,50 @@ classdef OCProblem < handle
          uHalf = obj.compute_midpoints(u);
          
          for i = 1:obj.nSteps
-            xk1 = obj.f(t(i), x(:,i), u(:,i));
-            xk2 = obj.f(tHalf(i), x(:,i) + .5*h*xk1, uHalf(:,i));
-            xk3 = obj.f(tHalf(i), x(:,i) + .5*h*xk2, uHalf(:,i));
-            xk4 = obj.f(t(i+1), x(:,i) + h*xk3, u(:,i+1));
-            x(:,i+1) = x(:,i) + h*(xk1 + 2*xk2 + 2*xk3 + xk4)/6;
+            y1 = obj.f(t(i), x(:,i), u(:,i));
+            xK(:,i,1) = x(:,i) + .5*h*y1;
             
-            objk1 = obj.g(t(i), x(:,i), u(:,i));
-            objk2 = obj.g(tHalf(i), x(:,i) + .5*h*xk1, uHalf(:,i));
-            objk3 = obj.g(tHalf(i), x(:,i) + .5*h*xk2, uHalf(:,i));
-            objk4 = obj.g(t(i+1), x(:,i) + h*xk3, u(:,i+1));
-            cumObj(i+1) = cumObj(i) + h*(objk1 + 2*objk2 + 2*objk3 + objk4)/6;
+            y2 = obj.f(tHalf(i), xK(:,i,1), uHalf(:,i));
+            xK(:,i,2) = x(:,i) + .5*h*y2;
+            
+            y3 = obj.f(tHalf(i), xK(:,i,2), uHalf(:,i));
+            xK(:,i,3) = x(:,i) + h*y3;
+            
+            y4 = obj.f(t(i+1), xK(:,i,3), u(:,i+1));
+            
+            x(:,i+1) = x(:,i) + h*(y1 + 2*y2 + 2*y3 + y4)/6;
+            
+            y1 = obj.g(t(i), x(:,i), u(:,i));
+            y2 = obj.g(tHalf(i), xK(:,i,1), uHalf(:,i));
+            y3 = obj.g(tHalf(i), xK(:,i,2), uHalf(:,i));
+            y4 = obj.g(t(i+1), xK(:,i,3), u(:,i+1));
+            
+            cumObj(i+1) = cumObj(i) + h*(y1 + 2*y2 + 2*y3 + y4)/6;
          end
          J = cumObj(end);         
       end
       
-      function lam = compute_adjoints(obj, u)
+      function lam = compute_adjoints(obj, x, xK, u)
+         lam = zeros(obj.nStates, obj.nSteps+1);
+         t = obj.tspan;
+         h = obj.h;
+         tHalf = obj.compute_midpoints(t);
+         uHalf = obj.compute_midpoints(u);
+         
+         for i = obj.nSteps:-1:1
+            y1 = obj.dfdx(t(i), x(:,i), u(:,i));
+            y2 = obj.dfdx(tHalf(i), xK(:,i,1), uHalf(:,i))*...
+               (eye(obj.nStates) + .5*h*y1);  % TODO check if matrix order is correct in multiplication
+            y3 = obj.dfdx(tHalf(i), xK(:,i,2), uHalf(:,i))*...
+               (eye(obj.nStates) + .5*h*y2);
+            y4 = obj.dfdx(t(i+1), xK(:,i,3), u(:,i+1))*...
+               (eye(obj.nStates) + h*y3);
+            
+            
+         end
+      end
+      
+      function dJdu = compute_objective_gradient(x, lam, u)
          
       end
    end
