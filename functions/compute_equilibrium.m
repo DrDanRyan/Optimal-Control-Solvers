3cbc83d203cfdb454905494ecce9dafca2e4e9ac
Daniel Ryan <danryanfl@yahoo.com>
2014-05-16 09:45:32 -0400

    start fixing compute_equilibrium

--- functions/compute_equilibrium.m
+++ functions/compute_equilibrium.m
@@ -1,4 +1,5 @@
-function [xStar, lamStar, uStar] = compute_equilibrium(prob, xGuess, lamGuess, uGuess)
+function [xStar, lamStar, uStar] = ...
+   compute_equilibrium(prob, xGuess, lamGuess, uGuess)
 
    nStates = length(xGuess);
    nControls = length(uGuess);
@@ -6,8 +7,10 @@ function [xStar, lamStar, uStar] = compute_equilibrium(prob, xGuess, lamGuess, u
    function value = equilibrium_system(y)
       x = y(1:nStates); lam = y(nStates+1:2*nStates); u = y(2*nStates+1:end);
       value = zeros(2*nStates+nControls, 1);
-      value(1:nStates) = prob.F(0, x, u);
-      value(nStates+1:2*nStates) = prob.dFdx_times_vec(0, x, u, lam);
+      F = prob.F(0, x, u);
+      value(1:nStates) = F(1:end-1,:);
+      dFdx_times_vec = prob.dFdx_times_vec(0, x, u, lam);
+      value(nStates+1:2*nStates) = dFdx_times_vec(1:end-1,:);
       value(2*nStates+1:end) = prob.dFdu_times_vec(0, x, u, lam);
    end
 
