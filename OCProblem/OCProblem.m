3259f9d818cc633f01a388e71ff30af5bd8a601f
DrDanRyan <danryanfl@yahoo.com>
2014-05-20 11:56:44 -0400

    Change tests to use new file names

--- OCProblem/OCProblem.m
+++ OCProblem/OCProblem.m
@@ -7,8 +7,15 @@ classdef OCProblem < handle
    
    methods (Abstract)
       
+      % This function computes the state equations RHS. The objective integrand
+      % should be the formula in the last row.
       dxdt = F(obj, t, y, u)
+      
+      % This function computes dFdx'*v. Because the objective variable, y(end),
+      % is not used to compute F, the last row of value will be zero.
       value = dFdx_times_vec(obj, t, y, u, v)
+      
+      % This function computes dFdu'*v.
       value = dFdu_times_vec(obj, t, y, u, v)      
       
    end
